de2185be3cc6160616b4eeaa0d4505bc2da01e2c
mth-692 <156521699+mth-692@users.noreply.github.com>
2024-01-13 23:09:26 +0700

    Update cakebank.js

--- cakebank.js
+++ cakebank.js
@@ -6,8 +6,8 @@ let boardHeight = 640;
 let context;
 
 //cake
-let cakeWidth = 51; //width/height ratio = 408/228 = 17/12
-let cakeHeight = 36;
+let cakeWidth = 44.3; //width/height ratio = 408/228 = 17/12
+let cakeHeight = 24.9;
 let cakeX = boardWidth/8;
 let cakeY = boardHeight/2;
 let cakeImg;
