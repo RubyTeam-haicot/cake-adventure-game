18fbfae365a38bfea8921835f8f38236ae2cff56
mth-692 <156521699+mth-692@users.noreply.github.com>
2024-01-14 14:09:57 +0700

    Update cakebank.js

--- cakebank.js
+++ cakebank.js
@@ -163,8 +163,8 @@ function movecake(e) {
 }
 
 function detectCollision(a, b) {
-    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-        a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-        a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-        a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
 }
